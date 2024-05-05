209765b0d9a39446227f8d2aec1473548afd9f9e
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2024-05-05 10:50:40 +0300

    AuthenticationService: Dockerfile

--- AuthenticationService/src/Index.ts
+++ AuthenticationService/src/Index.ts
@@ -3,8 +3,15 @@ import 'module-alias/register';
 import MainRouter from '@/Routes/index';
 import App from './App';
 import appConfig from './Helpers/appConfig';
-(async () => (console.log( (await appConfig.db()).elasticsearch?.node)))()
-const app = new App(new MainRouter(), Number(process.env.APPLICATION_PORT));
 
-app.listen();
+(async () => {
+  try {
+    const dbConfig = await appConfig.db();
+    console.log(dbConfig.elasticsearch?.node);
 
+    const app = new App(new MainRouter(), Number(process.env.APPLICATION_PORT));
+    app.listen();
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
